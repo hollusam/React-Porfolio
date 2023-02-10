a80f0e29133a2ecd40201ff053800033b5b6ddd0
Hollusam <oluyomisamuels@gmail.com>
2023-02-10 20:23:16 +0000

    Navbar component updated

--- react-portfolio/src/components/Navbar/index.js
+++ react-portfolio/src/components/Navbar/index.js
@@ -4,24 +4,27 @@ import './style.css';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-md">
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav mb-5 ml-auto top-nav">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home<span className="sr-only"></span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#about">About</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#projects">Projects</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+        <nav className="navbar navbar-expand-lg navbar-light"> 
+            <button class="navbar-toggler burger-menu" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon "></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav mb-5 ml-auto top-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home<span className="sr-only"></span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#about">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#projects">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 }
 
--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "../node_modules/jquery/dist/jquery.min.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
